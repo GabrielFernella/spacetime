48edbe8fe6ab51cff3167cfc8f384d3594203f2d
FernellaDev <gabrielfernella07@hotmail.com>
2023-05-18 02:30:40 -0300

    create right screen front

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua Capsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* left */}
+      <div></div>
+
+      {/* right */}
+      <div className="flex flex-1 items-center justify-center">
+        <p className="w-[360px] text-center leading-relaxed">
+          Você ainda não registrou nenhuma lembrança, começa{' '}
+          <a href="" className="underline hover:text-gray-50">
+            criar agora
+          </a>
+          !
+        </p>
+      </div>
+    </main>
   )
 }
