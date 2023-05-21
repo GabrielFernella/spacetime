b5a5a7705c5cd54eaa8c57b12ae67b611483c43d
GabrielDEV <gabrielfernella07@hotmail.com>
2023-05-21 16:08:17 -0300

    feature: add  route to send upload files

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,12 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
@@ -13,18 +16,23 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(multipart)
 app.register(jwt, {
   secret: 'spacetime', // add uma secret complexa
 })
 
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
     port: 3333,
-    host: '::',
-    // host: '0.0.0.0',
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('Start server in port http://localhost:3333')
